cbda2aa548d448ac9dab3a643e630e451ed919b6
Mariana Cavichioli <cavichiolimariana@gmail.com>
2017-10-11 15:08:46 -0300

    Código inicial

---
+++ app/src/main/java/com/example/mariana/agenda/ListaContatosActivity.java
@@ -0,0 +1,13 @@
+package com.example.mariana.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaContatosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_contatos);
+    }
+}
